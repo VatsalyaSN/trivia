f941c0bee24c4945001185067b1f2736d11ad0ad
VatsalyaSN <vatsalya.sn20@gmail.com>
2017-06-19 20:43:53 +0530

    form submission and wrong ans highlight

--- client/components/FormDisplay.js
+++ client/components/FormDisplay.js
@@ -1,36 +1,103 @@
 import React from 'react';
 
 const FormDisplay = React.createClass({
+	getInitialState(){
+		return {
+			cName:["none","none","none"],
+			msg : "",
+		}
+	},
+
+	renderCName(i){
+		return this.state.cName[i];
+	},
+
+	renderQuestion(item,i){
+		return <div>
+				 <p className={this.renderCName(i)} key={i}>{this.props.question[item].q}</p>
+				 <select id={item}>
+				 {this.props.question[item].o.map(function(opt,v){
+				 	return <option className="right" value={opt} key={v}>{opt}</option>
+				 })}
+
+				 </select>
+				 </div>
+	},
 
 	renderItem(question){
 		var arr = [];
 		for(var key in question){
 			arr.push(key)
 		}
-		return arr.map(function(item,i){
-			return(<div>
-				 <p>{question[item].q}</p>
-				 <select>
-				 {question[item].o.map(function(opt,i){
-				 	return <option value={opt}>{opt}</option>
-				 })}
-				 </select>
-				 </div>
-		)
-		})
+		return arr.map(this.renderQuestion)
+	},
+
+	checkAnswer(item,i){
+		var newArr=[];
+    	if(this.props.question[item].a !== this.state.answers[i])
+    		{
+    			newArr.push("wrong");
+    		}
+    	return newArr;
 	},
 
 	handleSubmit(e){
-		// e.preventDefault();
-		
-		// this.refs.form.reset();
+		e.preventDefault();
+		var arr = [];
+	    var newArr = [];
+		var elt;
+		var text=[];
+		for(var key in this.props.question){
+			arr.push(key)
+		}
+		arr.map(function(item,i){
+			elt = document.getElementById(item);
+			if (elt.selectedIndex == -1)
+	        	return null;
+
+	    	text.push(elt.options[elt.selectedIndex].text);
+    	})
+    
+	    for(var i=0;i<arr.length;i++){
+	    	console.log(text[i])
+	    	if(text[i] === "select")
+    		{
+    			alert("Please select answer")
+    			return;
+    		}
+    		
+    		else if(this.props.question[arr[i]].a !== text[i])
+    			{
+    				newArr.push("wrong");
+
+    			}
+
+    		else
+    			newArr.push("none")
+    	}
+	    this.setState({
+    			cName : newArr
+    		})
+	},
+
+	resetForm(){
+		this.refs.form.reset();
+		this.setState({
+				cName : ["none","none","none"],
+				msg : ""
+			})
 	},
 
 	render(){
 		return(
+			<div>
+			{this.state.msg !== ""? alert(this.state.msg) : ""}
 			<form className="form" ref="form" onSubmit={this.handleSubmit}>
 				{this.renderItem(this.props.question)}
+				<button className="btn-submit" type="submit">Submit</button>
 			</form>
+			<button className="btn-submit" onClick={()=>this.resetForm()}>Reset</button>
+			</div>
 			)
 	}
 })
--- client/data/Data.js
+++ client/data/Data.js
@@ -1,17 +1,17 @@
 const question = {
 	"1":{
 		"q" : "When was the last time India won the Cricket World Cup?",
-		"o" : ["2004", "2008", "2010", "2012"],
+		"o" : ["select","2004", "2008", "2010", "2012"],
 		"a" : "2010"
 	},
 	"2":{
 		"q" : "What is the highest individual score by a batsman in Test cricket?",
-		"o" : ["234","500","432","600"],
+		"o" : ["select","234","500","432","600"],
 		"a" : "432"
 	},
 	"3":{
 		"q":"Who has won the most number of cricket World Cups?",
-		"o":["Australia","India","Srilanka","England"],
+		"o":["select","Australia","India","Srilanka","England"],
 		"a":"Australia"
 	}
 
