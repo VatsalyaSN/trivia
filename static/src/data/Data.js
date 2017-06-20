f1c1bf2f8a2170536bfe750523cf909190270efd
VatsalyaSN <vatsalya.sn20@gmail.com>
2017-06-20 11:25:52 +0530

    updated trivia questions

--- static/src/data/Data.js
+++ static/src/data/Data.js
@@ -16,24 +16,24 @@ const question = {
 	}
 	,
 	"4":{
-		"q":"4. Who has won the most number of cricket World Cups?",
-		"o":["select","Australia","India","Srilanka","England"],
-		"a":"Australia"
+		"q":"4. Of the first seven World Cups, 1975 to 1999, which city has hosted the most numbers of finals?",
+		"o":["select","Capetown","London","Lahore","Melbourne"],
+		"a":"London"
 	},
 	"5":{
-		"q":"5. Who has won the most number of cricket World Cups?",
-		"o":["select","Australia","India","Srilanka","England"],
-		"a":"Australia"
+		"q":"5. Australian spinner Shane Warne passed a career milestone in this match. What was it?",
+		"o":["select","600 Wickets","Most wickets taken at Old Trafford","Highest bating score","Longest playing test bowler"],
+		"a":"600 Wickets"
 	},
 	"6":{
-		"q":"6. Who has won the most number of cricket World Cups?",
-		"o":["select","Australia","India","Srilanka","England"],
-		"a":"Australia"
+		"q":"6. In which Australian city is the Boxing Day Test played each year??",
+		"o":["select","Sydney","Hobart","Melbourne","Brisbane"],
+		"a":"Melbourne"
 	},
 	"7":{
-		"q":"7. Who has won the most number of cricket World Cups?",
-		"o":["select","Australia","India","Srilanka","England"],
-		"a":"Australia"
+		"q":"7. What sort of bowler was Shane Warne?",
+		"o":["select","Off Spinner","Lawn bowler","Leg spinner","Medium pacer"],
+		"a":"Leg spinner"
 	}
 }
 
